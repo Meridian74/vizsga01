e59ff40b875980c62e6d161189355d74f71970b2
meridian74 <meridian@freemail.hu>
2021-04-29 10:22:27 +0200

    all tests done

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,20 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    public int addDigits(String message) {
+        int sumOfDigits = 0;
+
+        if (message == null || message.length() == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < message.length(); i++) {
+            char nextLetter = message.charAt(i);
+            if (nextLetter >= '0' && nextLetter <= '9') {
+                int foundedNextDigit = Integer.parseInt(String.valueOf(nextLetter));
+                sumOfDigits += foundedNextDigit;
+            }
+        }
+        return sumOfDigits;
+    }
 }
